38da655a308339d14ccaedea82b0b76021465013
nagano564 <nagano564@gmail.com>
2017-03-02 22:07:34 -0500

    messages showing

--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -1,6 +1,8 @@
 (function () {
-    function MainCtrl($scope, Room, $uibModal) {
+    function MainCtrl($scope, Room, Message, $uibModal) {
         $scope.rooms = Room.all;
+        $scope.currentRoom = null;
+        $scope.messages = [];
         $scope.open = function() {
             var modalInstance = $uibModal.open({
                 templateUrl: '/templates/modal.html',
@@ -8,9 +10,14 @@
                 size: 'md'
             })
         }
+        
+        $scope.selectRoom = function(room){
+            $scope.currentRoom = room;
+            $scope.messages = Message.getByRoomId($scope.currentRoom.$id);
+        };
     }
 
     angular
         .module('blocChat')
-        .controller('MainCtrl', ['$scope', 'Room', '$uibModal', MainCtrl]);
+        .controller('MainCtrl', ['$scope', 'Room', 'Message', '$uibModal', MainCtrl]);
 })();
--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -13,7 +13,6 @@
                 templateUrl: '/templates/home.html'
               });
             
-            
         }
  angular
     .module('blocChat', ['ui.router', 'firebase', 'ui.bootstrap'])
